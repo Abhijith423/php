347a18b32a68315eab2fc17e64372fd5872fe2c2
Abhijith <abhijithemes@gmail.com>
2025-06-29 17:03:39 +0530

    variables.php

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php
+$name = "raj";
+echo "{$name}<br>";
+echo "hello <br>";
+echo "Hi {$name}<br>";
+echo "Welcome {$name}<br>";
+$age=35;
+echo "hi  my name is {$name} and  my age is {$age} <br>";
+$quantity=10;
+echo "this cocalcola tin has a quantity of {$quantity}ml <br>";
+$employed=true;
+echo "raj is employed {$employed}<br>";
+$employed=false;
+echo "raj is employed {$employed}<br>";
+?>
