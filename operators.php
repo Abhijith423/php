2b52ad666fc56b59dd2329bce33255ab590b4f58
Abhijith <abhijithemes@gmail.com>
2025-06-29 17:42:01 +0530

    ]operators.php

---
+++ operators.php
@@ -0,0 +1,22 @@
+<?php
+//Arithematic operator
+$x=5;
+$y=10;
+$z=null;
+$z=$x+$y;
+echo $z."<br>";
+$z=$x-$y;
+echo $z."<br>";
+$z=$x*$y;
+echo $z."<br>";
+$z=$x/$y;
+echo $z."<br>";
+$z=$x%$y;
+echo $z."<br>";
+#increment/decrement operator
+$x=10;
+echo $x++."<br>";
+echo $x--."<br>";
+$total=12*10+5*(5-10)/2;
+echo $total."<br>";
+?>
