618c557b75561e4f6d79a014c20f9f47467d3fa3
Abhijith <abhijithemes@gmail.com>
2025-06-29 13:42:18 +0530

    htmlinphp.php

---
+++ htmlinphp.php
@@ -0,0 +1,22 @@
+
+
+// adding html in php
+<?php
+    echo "Hello World";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+<button>Click Me</button>
+<br>
+<h1>Heading</h1>
+    
+</body>
+</html>
