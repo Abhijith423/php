2fb4eb42d365385219d7cf3131e09c1285c6ed0e
Abhijith <abhijithemes@gmail.com>
2025-07-04 10:22:41 +0530

    Register form.php

---
+++ Register form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Form</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form action="insert.php" method="POST">
+        <label>Username:</label>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password:</label>
+        <input type="password" name="password" required><br><br>
+
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
